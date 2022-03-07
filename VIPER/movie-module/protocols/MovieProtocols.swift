d6ecad946a513294806016542ea467b369922b8b
Hojin <khojin.ryu01@gmail.com>
2022-03-07 07:36:22 -0300

    setup comments

--- VIPER/movie-module/protocols/MovieProtocols.swift
+++ VIPER/movie-module/protocols/MovieProtocols.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+
+//ViewtoPresenterProtocol
+    // reference to router, view, interactor
+    // a function to just "ask" to fetch movie in interactor
+
+
+//PresenterToViewProtocol
+    // a function to returns a data that came from interactor
+        // can divide up into two
+            // 1. func success 2. func failure
+
+// PresenterToInteractorProtocol
+    // a function to really fetch movie in interactor
+
+
+// PresenterToRouterProtocol
+    // a function to create a module
+
+
+
+// InteractorToPresenterProtocol
+    // similar to presenter->view protocol but not the same.
+    // 1. func success 2. func failure
+
+
+
+
+
+    
