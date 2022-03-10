bacecd18d18113e4a74d5f0e3912d102f0d07fa3
HeitorKun <60748906+HeitorKun@users.noreply.github.com>
2022-03-10 15:20:15 -0300

    working on the cells

---
+++ VIPER/movie-module/view/MoviesListTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MoviesListTableViewCell.swift
+//  VIPER
+//
+//  Created by Heitor Feijó Kunrath on 10/03/22.
+//
+
+import UIKit
+
+class MoviesListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
